05bb8ac4994c92a6e32abc987d2e77d918de980d
Mehmetcil <mehmetcil096@gmail.com>
2023-01-01 01:39:30 +0300

    day11

--- src/test/java/practice/Q04.java
+++ src/test/java/practice/Q04.java
@@ -1,4 +1,63 @@
 package practice;
 
-public class Q04 {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import utilities.TestBase;
+
+import static org.junit.Assert.assertTrue;
+
+public class Q04 extends TestBase {
+
+    //              ..Exercise2...
+    //  http://www.bestbuy.com 'a gidin,
+    //  Sayfa basliginin "Best" icerdigini(contains) dogrulayin
+    //  Ayrica Relative Locator kullanarak;
+    //  logoTest => BestBuy logosunun goruntulenip goruntulenmedigini dogrulayin
+    //  Ayrica Relative Locator kullanarak;
+    //  mexicoLinkTest => Linkin goruntulenip goruntulenmedigini dogrulayin
+
+    @Before
+    public void beforeTest () {
+        driver.get("http://www.bestbuy.com");
+    }
+
+    @Test
+    public void titltTest () {
+        // Sayfa basliginin "Best" icerdigini(contains) dogrulayin
+
+        String expTitle = "Best";
+        String actTitle = driver.getTitle();
+
+        boolean iceriyorMu = actTitle.contains(expTitle);
+        assertTrue(iceriyorMu);
+    }
+
+    @Test
+    public void logoTest () {
+
+        //  Ayrica Relative Locator kullanarak;
+        //  logoTest => BestBuy logosunun goruntulenip goruntulenmedigini dogrulayin
+
+        WebElement helloHeading = driver.findElement(By.xpath("(//div[@class='heading'])[1]"));
+        WebElement bestBuyLogo = driver.findElement(RelativeLocator.with(By.tagName("img")).above(helloHeading));
+
+        assertTrue(bestBuyLogo.isDisplayed());
+    }
+
+    @Test
+    public void mexicoLinkTest () {
+
+        //  Ayrica Relative Locator kullanarak;
+        //  mexicoLinkTest => Linkin goruntulenip goruntulenmedigini dogrulayin
+
+        WebElement usFlag = driver.findElement(By.xpath("(//img[@alt='United States'])[1]"));
+        WebElement maxicoFlag = driver.findElement(RelativeLocator.with(By.tagName("img")).toRightOf(usFlag));
+
+        boolean goruntuleniyorMu = maxicoFlag.isDisplayed();
+        assertTrue(goruntuleniyorMu);
+    }
 }
